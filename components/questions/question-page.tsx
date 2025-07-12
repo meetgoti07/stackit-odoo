070c6ece9b8b8b64d19196eb71086478b8fdc0a8
Urmil Gadhiya <126799037+urmilgadhiya18@users.noreply.github.com>
2025-07-12 16:42:12 +0530

    question-page bug fixes

--- components/questions/question-page.tsx
+++ components/questions/question-page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { useState, use } from "react"
 import { Button } from "@/components/ui/button"
 import { RichTextEditor } from "@/components/rich-text-editor/rich-text-editor"
