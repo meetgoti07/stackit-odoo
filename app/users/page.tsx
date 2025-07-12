f9d02f703e2ba9d78df32eba276ae6caaee2417f
Krish Gangajaliya <krishgangajaliya1@gmail.com>
2025-07-12 16:40:02 +0530

    Update users/page.tsx and fixed bugs

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,15 +1,17 @@
 "use client"
-
+// importing necessary libraries
 import { Header } from "@/components/header/header"
 import { Sidebar } from "@/components/sidebar/sidebar"
 import { RightSidebar } from "@/components/sidebar-right/right-sidebar"
 import { UsersContent } from "@/components/users/users-content"
-
+// main function
 export default function UsersPage() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <div className="max-w-7xl mx-auto flex">
+{/*         using all the necessary components */}
+
         <Sidebar />
         <UsersContent />
         <RightSidebar />
