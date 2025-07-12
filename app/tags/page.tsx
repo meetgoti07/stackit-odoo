14b31915804acdd3f7b33a0e87e719be7ed7db8f
Krish Gangajaliya <krishgangajaliya1@gmail.com>
2025-07-12 16:34:11 +0530

    Update tags/page.tsx and fixed bugs

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -1,15 +1,16 @@
 "use client"
-
+// import necessary libraries 
 import { Header } from "@/components/header/header"
 import { Sidebar } from "@/components/sidebar/sidebar"
 import { RightSidebar } from "@/components/sidebar-right/right-sidebar"
 import { TagsContent } from "@/components/tags/tags-content"
-
+// main page
 export default function TagsPage() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <div className="max-w-7xl mx-auto flex">
+{/*         using all imported components */}
         <Sidebar />
         <TagsContent />
         <RightSidebar />
